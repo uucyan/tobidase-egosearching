87d91ecc03d77a96d83b91f63da372765ca26f38
uucyan <uuunouucyan@gmail.com>
2018-09-01 19:24:42 +0900

    scss と Element を使えるようにした

--- pages/index.vue
+++ pages/index.vue
@@ -17,22 +17,23 @@
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey">GitHub</a>
+        <el-button type="primary" icon="el-icon-search">Search</el-button>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from "~/components/AppLogo.vue";
 
 export default {
   components: {
     AppLogo
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
 .container {
   min-height: 100vh;
   display: flex;
@@ -42,7 +43,9 @@ export default {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
@@ -62,4 +65,3 @@ export default {
   padding-top: 15px;
 }
 </style>
-
---
+++ plugins/element-ui.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+
+const locale = require('element-ui/lib/locale/lang/ja')
+Vue.use(ElementUI, {
+  locale
+})
